a32d9331ba1d0c36fb5c712195cd04ade3a143d1
Matthew Jones <matthewbtjones2000@gmail.com>
2021-11-03 14:27:15 +0000

    Fixed type for ANSI compliance

--- helpers/getInput.h
+++ helpers/getInput.h
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 
-void getInput(char * filename, int *destArr, int *arrSize)
+void getInput(char *filename, int *destArr, int *arrSize)
 {
-    FILE* f = fopen(filename, "r");
+    FILE *f = fopen(filename, "r");
     size_t linesize = 10;
     char *line = malloc(sizeof(char) * linesize);
-    ssize_t read = 0;
+    size_t read = 0;
     int i = 0;
 
-    while((read = getline(&line, &linesize, f)) != -1) {
+    while ((read = getline(&line, &linesize, f)) != -1)
+    {
         int val = atoi(line);
         destArr[i++] = val;
     }
 
     *arrSize = i;
-    free(line); 
+    free(line);
 }
